1da3e0fe1b8c29c7743a82c55a0c7a6958253ee8
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-03 22:27:08 -0600

    Lay out storyboard. Extend Bundle

--- InAPeace/ViewController.swift
+++ InAPeace/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var quoteImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
